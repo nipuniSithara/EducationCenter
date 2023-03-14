8dc49e3b507e1ee1092600e5bd0f8f0cbee1690f
LAPTOP-IURL2MHE\Nipuni <nipuanandawansha@gmail.com>
2023-03-14 18:19:06 +0530

    css changing

--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -5,7 +5,12 @@ export const Home = () => {
     return(
         <div className="d-flex flex-column" >
             <div><NavBar /></div>
-            <div ><img src={banner}/></div>
+            <div ><img src={banner} width="100%" /></div>
+            <div className="row tiles-container">
+                <div className="col tile">A</div>
+                <div className="col tile">B</div>
+                <div className="col tile">C</div>
+            </div>
         </div>
     )
 }
