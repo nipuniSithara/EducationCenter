3a5909093675246d1417cee0fc855e41a5f732a9
LAPTOP-IURL2MHE\Nipuni <nipuanandawansha@gmail.com>
2023-03-15 12:03:26 +0530

    creating header

--- Frontend/src/pages/allCourses.jsx
+++ Frontend/src/pages/allCourses.jsx
@@ -1,6 +1,32 @@
 
 export const AllCourses = () => {
     return(
-        <>AllCourses</>
+        <div className="main-container">
+            <div className="d-flex flex-row justify-content-between"> 
+                <div className="col-5 d-flex flex-row justify-content-between">
+                    <label for="inputEmail3" class="col-sm-4 col-form-label">Department</label>
+                    <div class="col-sm-8">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>All</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="col-5 d-flex flex-row justify-content-between">
+                    <label for="inputEmail3" class="col-sm-4 col-form-label">Level</label>
+                    <div class="col-sm-8">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>All</option>
+                            <option value="1">Level - 1</option>
+                            <option value="2">Level - 2</option>
+                            <option value="3">Level - 3</option>
+                            <option value="4">Level - 4</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
